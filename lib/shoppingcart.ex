63c324656daf98c85d2ced9bfb3e0aca9c248160
Julia Gao <juliagao2005@gmail.com>
2015-08-29 11:11:36 -0600

    passed some tests

--- lib/shoppingcart.ex
+++ lib/shoppingcart.ex
@@ -2,4 +2,7 @@ defmodule ShoppingCart do
   def list do
     []
   end
+  def addItem do
+    [] ++ [1]
+  end
 end
--- test/shoppingcart_test.exs
+++ test/shoppingcart_test.exs
@@ -4,4 +4,12 @@ defmodule ShoppingCartTest do
   test "initial state" do
     assert (length ShoppingCart.list) == 0
   end
+
+  test "test one item" do
+    assert (length ShoppingCart.addItem) == 1
+  end
+
+  test "test add array" do
+    assert [] ++ [1] == [1]
+  end
 end
