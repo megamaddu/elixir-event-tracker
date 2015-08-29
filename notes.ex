3bcd052148cc1115d013b8612a655df6b2c5be10
Dan Otto <theDanOtto@gmail.com>
2015-08-29 10:34:00 -0600

    added notes

---
+++ notes.ex
@@ -0,0 +1,40 @@
+What problem are we solving?
+We are building a form information tracker.
+We are not building a form information tracker.
+We are building a cart! :DDDDDDDDDDDDD!!!!!!!!! :)))))))
+
+ShoppingCart
+  Core Functionality
+
+    Events:
+      Add to Cart
+      Remove from Cart
+      -> but this doesnt really remove it from cart, it makes a new entry saying that it's removed
+
+    What gets added to the cart?
+    Items!
+      What does an item have?
+      quantity, price, name
+      
+    Outputs:
+    core outputs
+    1) is a list of all Events
+    2) whats actually in the cart now
+    3) total price (or calculate on your own)
+
+
+    awesomeness outputs
+    4) what are all the things that were removed from the cart?
+    5) ...
+
+
+    This was put in, this was taken out...
+
+Programming goal...
+
+
+
+We may want to use tuples for the stuff in the cart.
+
+If we really get awesome!!!
+  An ability for a user to save an item for later.
