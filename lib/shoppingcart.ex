375f11feac001f39fdaec54ca794fb9167e245c9
Michael Trotter <spicydonuts@gmail.com>
2015-08-29 13:28:25 -0600

    genevent

--- lib/shoppingcart.ex
+++ lib/shoppingcart.ex
@@ -1,8 +1,45 @@
 defmodule ShoppingCart do
-  def list do
+  use GenEvent
+
+  # Cart Actor
+  def init do
+    {:ok, pid} = GenEvent.start_link
+    GenEvent.add_handler pid, ShoppingCart, []
+    pid
+  end
+
+  def list(pid) do
+    GenEvent.call pid, ShoppingCart, :list_items
+  end
+
+  def add(pid, item) do
+    GenEvent.notify pid, {:add_item, item}
+  end
+
+  def remove(pid, item) do
+    GenEvent.notify pid, {:remove_item, item}
+  end
+  # End Cart Actor
+
+  # GenEvent
+  def handle_event(event, events) do
+    {:ok, [event|events]}
+  end
+
+  def handle_call(action, events) do
+    case action do
+      :list_items -> {:ok, Enum.reverse(events), events}
+    end
+  end
+  # End GenEvent
+
+  # Cart Logic
+  def _list do
     []
   end
-  def addItem do
-    [] ++ [1]
+
+  def _addItem(cart, item) do
+    cart ++ [item]
   end
+  # End Cart Logic
 end
--- test/shoppingcart_test.exs
+++ test/shoppingcart_test.exs
@@ -2,14 +2,39 @@ defmodule ShoppingCartTest do
   use ExUnit.Case
 
   test "initial state" do
-    assert (length ShoppingCart.list) == 0
+    pid = ShoppingCart.init
+    assert (length (ShoppingCart.list pid)) == 0
   end
 
   test "test one item" do
-    assert (length ShoppingCart.addItem) == 1
+    pid = ShoppingCart.init
+    ShoppingCart.add pid, {:item1}
+    assert (length (ShoppingCart.list pid)) == 1
+    ShoppingCart.add pid, {:item2}
+    assert (length (ShoppingCart.list pid)) == 2
   end
 
   test "test add array" do
     assert [] ++ [1] == [1]
   end
 end
+
+
+
+
+# {:ok, pid} = GenEvent.start_link()
+#
+# GenEvent.add_handler(pid, LoggerHandler, [])
+# #=> :ok
+#
+# GenEvent.notify(pid, {:log, 1})
+# #=> :ok
+#
+# GenEvent.notify(pid, {:log, 2})
+# #=> :ok
+#
+# GenEvent.call(pid, LoggerHandler, :messages)
+# #=> [1, 2]
+#
+# GenEvent.call(pid, LoggerHandler, :messages)
+# #=> []
